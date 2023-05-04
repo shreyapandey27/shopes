a757a61ec40ca8cf0212ae623a3e352fa321e368
Shreya Pandey <shreyapandey281268@gmail.com>
2023-05-04 10:45:19 +0530

    adding more data in shopes to see update

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
 console.log("index file inside shopes");
 console.log("adding data in shopes");
 console.log("adding some more data in shopes");
+console.log("adding some more data in shopes to see update");
